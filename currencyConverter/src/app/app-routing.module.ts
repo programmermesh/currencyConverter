d2f32563b1c9e7219199d71e042d58b3f574081c
Meshach Joseph <pstmeshach@gmail.com>
2022-11-22 19:27:19 +0100

    configure routes
    Configuring the routes

--- currencyConverter/src/app/app-routing.module.ts
+++ currencyConverter/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CurrencyDetailsPageComponent } from './components/currency-details-page/currency-details-page.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full",
+  },
+  {
+    path: "home",
+    component: HomeComponent,
+  },
+  {
+    path: "currency-details-page",
+    component: CurrencyDetailsPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
